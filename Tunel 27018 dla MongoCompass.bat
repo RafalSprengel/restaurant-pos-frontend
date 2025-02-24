a844296c2cefc359e66b9e8abb6f7a63cddd2bd7
Rafal Sprengel <sprengel.rafal@gmail.com>
2025-02-24 20:51:11 +0000

    changes

--- Tunel 27018 dla MongoCompass.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-echo Uruchamianie tunelu SSH do MongoDB...
-ssh -p 2222 -L 27018:localhost:27017 rafal@188.68.242.226
-
-echo Tunel zamknięty.
-pause
--- Tunel 9229 dla DevTools inspect.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-echo Uruchamianie tunelu SSH...
-ssh -p 2222 -L 9229:localhost:9229 rafal@188.68.242.226
-
-echo Tunel SSH został zamknięty.
-pause
